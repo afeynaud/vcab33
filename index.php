f3aa0ffe39fe30be7c5e7458c2c6834782af449b
afeynaud <alexandre.feynaud@gmail.com>
2021-10-03 17:56:32 +0200

    Issues corrections + convert all files to php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html lang='fr'>
+
+<head>
+    <link rel='stylesheet' href='css/style.css'>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Vcab 33 : chauffeurs privés à Bordeaux et en Gironde</title>
+</head>    
+
+<body>
+
+<?php require './templates/header.php' ?>
+<?php require './templates/nav-menu.php' ?>
+
+        <div class='flexReservations'>
+
+            <div class='flexReservationsOne'>
+                <div class='flexLeft'>
+                    <h2 class='flexReservationsTitle'>Réservations et tarifs</h2>
+                    <p class='flexReservationsParagraph'>Nos chauffeurs sont à votre <br> disposition à partir de 20 €.</p>
+                    <button class='buttonReservations'>Réserver et voir le détail des tarifs</button>
+                </div>
+
+                <div class='flexRight'>
+                    <div class='reservationsImgOne'>
+                        <img src='https://i.imgur.com/Id1FodU.jpg' alt='chauffeur-prive-berline'>
+                    </div>
+                </div>
+            </div>
+
+            <div class='flexReservationsTwo'>
+                <div class='flexLeft'>
+                    <h2 class='flexReservationsTitle'>Réservations et tarifs</h2>
+                    <p class='flexReservationsParagraph'>Nos chauffeurs sont à votre <br> disposition à partir de 20 €.</p>
+                    <button class='buttonReservations'>Réserver et voir le détail des tarifs</button>
+                </div>
+
+                <div class='flexRight'>
+                    <div class='reservationsImgOne'>
+                        <img src='https://i.imgur.com/Id1FodU.jpg' alt='chauffeur-prive-berline'>
+                    </div>
+                </div>
+            </div>
+
+            <div class='flexReservationsThree'>
+                <div class='flexLeft'>
+                    <h2 class='flexReservationsTitle'>Réservations et tarifs</h2>
+                    <p class='flexReservationsParagraph'>Nos chauffeurs sont à votre <br> disposition à partir de 20 €.</p>
+                    <button class='buttonReservations'>Réserver et voir le détail des tarifs</button>
+                </div>
+
+                <div class='flexRight'>
+                    <div class='reservationsImgOne'>
+                        <img src='https://i.imgur.com/Id1FodU.jpg' alt='chauffeur-prive-berline'>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <?php require './templates/form.php' ?>
+</body>
+</html> 
---
+++ templates/form.php
@@ -0,0 +1,45 @@
+
+    <div class="container">
+        <div class="title">Devis & Contact</div>
+        <div class=class="content">
+            <form action="#">
+                <div class="user-details">
+
+                    <div class='input-box'>
+                        <label for="name" class="details">Nom / Prénom</label>
+                        <input type="text" id="name" placeholder="Enter your name" required>
+                    </div>
+
+                    <div class='input-box'>
+                        <label for="mail" class="details">Adresse mail</label>
+                        <input type="text" id="mail" placeholder="Enter your mail" required>
+                    </div>
+
+                    <div class='input-box'>
+                        <label for="phone" class="details">Téléphone</label>
+                        <input type="text" id="phone" placeholder="Enter your phone" required>
+                    </div>
+
+                    <div class='input-box'>
+                        <label for="departure-hours" class="details">Horaire de départ</label>
+                        <input type="text" id="departure-hours" placeholder="Enter your schedule" required>
+                    </div>
+
+                    <div class='input-box'>
+                        <label for="home-adress" class="details">Adresse de prise en charge</label>
+                        <input type="text" id="home-adress" placeholder="Enter your adress" required>
+                    </div>
+
+                    <div class='input-box'>
+                        <label for="destination-adress" class="details">Adresse de destination</label>
+                        <input type="text" id="destination-adress" placeholder="Enter your destination" required>
+                    </div>
+
+                </div>
+
+                <div class="buttons">
+                    <input type="submit" value="Réserver">
+                </div>
+            </form>
+        </div>
+    </div>
