d2d3303cbfb31437d32e683ea9a4d6853cc1aced
afeynaud <alexandre.feynaud@gmail.com>
2021-10-06 17:24:10 +0200

    resolve dangerous css and cards

--- src/pages/reservation-tarifs.php
+++ src/pages/reservation-tarifs.php
@@ -16,28 +16,25 @@
     require '../../templates/nav-menu.php';
     require '../../data/runs-global-informations.php';
 ?>
-<?php/* var_dump($runsPrestationsDetails);die */?>
-
 <h1>Réservation et tarifs</h1>
-<h2>Nos gammes de transport</h2>
-      </div>
-      <main class="cards">
+<h2 class="container">Nos gammes de transport</h2>
+      <main class="container cards">
         <article class="card">
-          <img src="https://picsum.photos/id/1003/300" alt="Sample photo">
+          <img class="card" src="https://picsum.photos/id/1003/300" alt="Sample photo">
           <div class="text">
             <h3>GAMME ECO</h3>
             <p>Voyagez à bord d’une berline économique 2 places vous procurant tout le confort nécessaire pour profiter au mieux de votre trajet.</p>        
           </div>
         </article>
         <article class="card">
-          <img src="https://picsum.photos/id/1003/300" alt="Sample photo">
+          <img class="card" src="https://picsum.photos/id/1003/300" alt="Sample photo">
           <div class="text">
             <h3>GAMME VIP</h3>
             <p>Avec l’offre VIP, nous vous transportons dans un véhicule haut de gamme avec vitres teintées. Le service à bord est personnalisé selon vos exigences et votre conducteur habilité à assurer votre sécurité.</p>           
           </div>
         </article>
         <article class="card">
-          <img src="https://picsum.photos/id/1003/300" alt="Sample photo">
+          <img class="card" src="https://picsum.photos/id/1003/300" alt="Sample photo">
           <div class="text">
             <h3>GAMME VAN</h3>
             <p>Notre gamme VAN est spécialement adaptée au transport de groupe. Nous vous garantissons bien évidemment un service à la hauteur de notre réputation.</p>
@@ -48,12 +45,11 @@
         <div class="presta">
           <h2>Prestations incluses</h2>
         </div>
-          <div>
-            <i class="fas fa-tint fa-3x"></i>
-            <i class="fas fa-wifi fa-3x"></i>
-            <i class="fas fa-battery-full fa-3x"></i>
+          <div class="container">
+            <div class="card-icons-blocks"><i class="fas fa-tint fa-3x card-circle-icon"></i></div>
+            <div class="card-icons-blocks"><i class="fas fa-wifi fa-3x card-circle-icon"></i></div>
+            <div class="card-icons-blocks"><i class="fas fa-battery-full fa-3x card-circle-icon"></i></div>
           </div>
-        </div>
       </main>
 
 <h2>Nos gammes de transports</h2>
@@ -64,10 +60,10 @@
 
 <h2>Exemples de trajets</h2>
 
-<?php
-    require '../../templates/form.php';
-    require '../../templates/footer.php';
-?>
+<div class="container-form1">
+    <?php require '../../templates/form.php';?>
+</div>
+<?php require '../../templates/footer.php';?>
 
 </body>
 </html>
