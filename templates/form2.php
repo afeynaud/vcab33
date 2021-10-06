a99251d64b8efa5de38d09eeabb8c9b14a404e5c
chbville <bonneville.charles@yahoo.fr>
2021-10-06 15:27:09 +0200

    form/tarifs/css

---
+++ templates/form2.php
@@ -0,0 +1,42 @@
+<div class='container'>
+            <form action="#">
+                <h2>Devis & Contact</h2>
+                <div class="user-details">
+
+                    <div class='input-box'>
+                        <label for="name" class="details">Nom / Prénom</label>
+                        <input type="text" id="name" placeholder="Enter your name" required>
+                    </div>
+
+                    <div class='input-box'>
+                        <label for="mail" class="details">Adresse mail</label>
+                        <input type="text" id="mail" placeholder="Enter your mail" required>
+                    </div>
+
+                    <div class='input-box'>
+                        <label for="phone" class="details">Téléphone</label>
+                        <input type="text" id="phone" placeholder="Enter your phone" required>
+                    </div>
+
+                    <div class='input-box'>
+                        <label for="departure-hours" class="details">Horaire de départ</label>
+                        <input type="text" id="departure-hours" placeholder="Enter your schedule" required>
+                    </div>
+
+                    <div class='input-box'>
+                        <label for="home-address" class="details">Adresse de prise en charge</label>
+                        <input type="text" id="home-address" placeholder="Enter your address" required>
+                    </div>
+
+                    <div class='input-box'>
+                        <label for="destination-address" class="details">Adresse de destination</label>
+                        <input type="text" id="destination-address" placeholder="Enter your destination" required>
+                    </div>
+
+                </div>
+
+                <div class="buttons">
+                    <input type="submit" value="Réserver">
+                </div>
+            </form>
+        </div>
