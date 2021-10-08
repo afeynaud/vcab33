41998bea250dc8f18a2177f11720b9369b83bae3
mathis <chartouni.mathis@gmail.com>
2021-10-08 10:19:15 +0200

    Modified pages and added images

--- src/pages/devenir-partenaires.php
+++ src/pages/devenir-partenaires.php
@@ -15,9 +15,69 @@
 <?php require '../../templates/nav-menu.php' ?>
 
 <h1>Devenir partenaires</h1>
+
+<section>
+
+        <div class='container'>
+                <div class='flex-item-left'>
+                    <h2>Une entreprise humaine</h2>
+                    <p>Nos chauffeurs sont salariés</br>
+                    et non travailleurs indépendants.</br>
+                    Nous formons une équipe de professionnels.</br></p>
+                </div>
+
+                <div class='flex-item-right'>
+                    <div class='img-margin-left'>
+                        <img src='https://i.imgur.com/H4B0HU0.jpg' alt='chauffeur-prive-entreprise-humaine'>
+                    </div>
+                </div>
+            </div>
+
+        <div class='container container-reversed'>
+            <div class='flex-item-left'>
+                <div class='s'>
+                    <img src='https://i.imgur.com/gFRFzzj.jpg' alt='chauffeur-prive-accompagnement'>
+                </div>
+            </div>
+
+            <div class='flex-item-right'>
+                <h2>Notre accompagnement</h2>
+                <p>Nous prenons en charge vos futures formations.</br>
+                Nous vous accompagnons tout au long de votre parcours.</br></p>
+            </div>
+        </div>
+
+        <div class='container'>
+                <div class='flex-item-left'>
+                    <h2>Sécurité</h2>
+                    <p>Nous assurons tous nos chauffeurs</br>et garantissons un trajet sûr à nos clients.</br></p>
+                </div>
+
+                <div class='flex-item-right'>
+                    <div class='img-margin-left'>
+                        <img src='https://i.imgur.com/FIQ9mDJ.jpg' alt='chauffeur-prive-securite'>
+                    </div>
+                </div>
+            </div>
+
+        <div class='container container-reversed'>
+            <div class='flex-item-left'>
+                <div class='s'>
+                    <img src='https://i.imgur.com/wzG09sx.jpg' alt='chauffeur-prive-proximite'>
+                </div>
+            </div>
+
+            <div class='flex-item-right'>
+                <h2>Proximité</h2>
+                <p>Nous sommes pleinement intégrés dans le tissu</br>économique et social de la région.</br></p>
+            </div>
+        </div>
+
+    </section>
+
 <div class="offres">
 <h2>Nos offres d'emploi</h2>
-<ul>
+<ul class='proper-font'>
     <li>Chauffeur V.I.P (Uniquement sur dossier) (C.D.I).</li>
     <li>Chauffeur polyvalent (Uniquement trajet éco et Van) (C.D.D).</li>
     <li>Employé de maintenance des véhicules (C.D.I)</li>
